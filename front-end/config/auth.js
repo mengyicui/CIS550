79a61c3103f8501a0506cbc7e9a6be165ec2d3f0
Zhengxuan Wu <zxw0424@gmail.com>
2017-03-20 17:35:38 -0400

    adding front end module

---
+++ front-end/config/auth.js
@@ -0,0 +1,17 @@
+module.exports = {
+  facebookAuth: {
+    clientID: '896605267054808',
+    clientSecret: '28f6a7d2842d5f9ba19e449131694902',
+    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+  },
+  twitterAuth: {
+    consumerKey: 'sOM9aZpoEyNAXKqvEUHd2O7cN',
+    consumerSecret: '6I1x0PgH41AxuqtAKcfRqYtWDMKNv0TBaaoegW3RP3o1ks7NPk',
+    callbackURL: 'http://localhost:3000/auth/twitter/callback',
+  },
+  googleAuth: {
+    clientID: '678868615990-p78thk2dabgjdnn54gabp18dmlb8pbdb.apps.googleusercontent.com',
+    clientSecret: 'luTbqVgVYvRcbASXsSo9qKCL',
+    callbackURL: 'http://localhost:3000/auth/google/callback',
+  },
+};
---
+++ front-end/routes/index.js
@@ -0,0 +1,104 @@
+var express = require('express');
+var passport = require('passport');
+var router = express.Router();
+
+var Twitter = require('twitter');
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login.ejs', { message: req.flash('loginMessage') });
+});
+
+router.get('/signup', function(req, res) {
+  res.render('signup.ejs', { message: req.flash('signupMessage') });
+});
+
+router.get('/profile', isLoggedIn, function(req, res) {
+  res.render('profile.ejs', { user: req.user });
+});
+
+// modified logout function
+router.get('/logout', function(req, res) {
+  req.session.destroy(function (err) {
+    res.redirect('/');
+  });
+});
+
+router.post('/signup', passport.authenticate('local-signup', {
+  successRedirect: '/profile',
+  failureRedirect: '/signup',
+  failureFlash: true,
+}));
+
+router.post('/login', passport.authenticate('local-login', {
+  successRedirect: '/profile',
+  failureRedirect: '/login',
+  failureFlash: true,
+}));
+
+router.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
+
+router.get('/auth/facebook/callback', passport.authenticate('facebook', {
+  successRedirect: '/profile',
+  failureRedirect: '/',
+}));
+
+router.get('/auth/twitter', passport.authenticate('twitter'));
+
+router.get('/auth/twitter/callback', passport.authenticate('twitter', {
+  successRedirect: '/twitterfeeds',
+  failureRedirect: '/',
+}));
+
+// display twitter feeds of this user
+router.get('/twitterfeeds', function(req, res) {
+
+  var client = new Twitter({
+  consumer_key: 'vZ1KblNI6Q29fDs6abo9Ozgy4',
+  consumer_secret: 'cModj4jzV51P1ywQPi2yHdNqfBNm5VdVWJwceIv5rCytOWFoHF',
+  access_token_key: req.user.token,
+  access_token_secret: req.user.tokenSecret
+  });
+  //console.log(req.user)
+  var params = {screen_name: req.user.twitter.displayName};
+
+  var results = [];
+  client.get('search/tweets', {q: 'usedcars'}, function(error, tweets, response) {
+    if (!error) {
+      console.log(tweets.statuses[0].user.screen_name)
+      var i;
+      for (i = 0; i < 5; i++) {
+        results.push({ text : tweets.statuses[i].text, username: tweets.statuses[i].user.screen_name });
+        //console.log(results)
+      }
+      //res.render('test.ejs');
+      res.render('searchmain.ejs', 
+            {feeds: results}
+      );
+    }
+  });
+});
+
+// display out the search result page
+router.get('/search', function(req, res) {
+  // trigger bing search here
+  res.render('searchresults.ejs');
+});
+
+router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+router.get('/auth/google/callback', passport.authenticate('google', {
+  successRedirect: '/profile',
+  failureRedirect: '/',
+}));
+
+module.exports = router;
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated())
+      return next();
+  res.redirect('/');
+}
